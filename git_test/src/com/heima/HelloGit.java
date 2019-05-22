bdb143e8cfee3fcfa19ac6ba565b54a9d2960e9b
wyg <wyg_0930@163.com>
2019-05-22 16:06:09 +0800

    添加工程到git

---
+++ git_test/src/com/heima/HelloGit.java
@@ -0,0 +1,9 @@
+package com.heima;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
